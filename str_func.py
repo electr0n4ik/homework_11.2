64ec70eb4c12e443373b495576011457ad4a85dd
electr0n4ik <andreyshka3@gmail.com>
2023-04-17 08:13:55 +0300

    UPD file:
    str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def get_large_letter(letters):
+    """
+    Функция делает первую букву каждого слова заглавной.
+    """
     return letters.upper()
+
+
+def get_title_words(string_):
+    """
+    Функция делает первую букву каждого слова заглавной.
+    """
+    return string_.title()
